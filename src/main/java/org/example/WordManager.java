bd2ac6d11eb58f14f7b9fb1b5609ad2a56bd8c34
hanminyee <hanmin0786@gmail.com>
2023-09-03 16:54:52 +0900

    종료기능 추가

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -25,13 +25,17 @@ public class WordManager {
     public void start(){
         while(true){
             int menu = selectMenu();
-            if (menu == 0) break;
+            if (menu == 0) {
+                System.out.println("프로그램 종료! 다음에 만나요~");
+                break;
+            }
             if(menu == 4){
                 wordCRUD.addWord();
             }
             else if(menu == 1) {
                 wordCRUD.listAll();
             }
+
         }
 
     }
